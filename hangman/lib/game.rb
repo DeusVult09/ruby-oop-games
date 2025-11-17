201943c3eef60242376c2c0fd7bf48ea71b9ecda
Zarifa <mamadalievazara@gmail.com>
2025-11-17 23:37:45 +0100

    Add methods to show_rules, player_setup, randomply select a word in a game class. Add: method to make a guess in a player class.

--- hangman/lib/game.rb
+++ hangman/lib/game.rb
@@ -3,6 +3,38 @@ require_relative 'save_game'
 require_relative 'player'
 
 class Game
-  
+  def initialize
+
+  end
+
+  def show_rules
+    puts <<~RULES
+      Welcome to Hangman game! The computer(host) will randomly choose a secret word between 5 and 12 letters.
+      Your goal is to guess the word one letter at a time.
+      You start with 7 incorrect guesses allowed.
+      Each time you guess a letter that is not in the word, you lose one life.
+      If you reveal all letters before running out of lives — you win!
+      If you use all 7 incorrect guesses — you lose.
+      You can also save your game at any time and load it later.
+      Do you want to continue? (yes/no)
+    RULES
+    input = gets.chomp.downcase
+    exit unless input == 'yes'
+  end
+
+  def player_setup
+    puts 'Dear player, enter your name:'
+    @name = gets.chomp
+  end
+
+  def select_word
+    random_word = File.readlines('words.txt', chomp: true)
+    filtered_word = random_word.select { |w| w.length.between?(5, 12) }
+    secret_word = filtered_word.sample
+  end
+
+  def display_feedback
+    puts 
+  end
 end
 
--- hangman/lib/player.rb
+++ hangman/lib/player.rb
@@ -1,6 +1,18 @@
+require_relative 'game'
+
 class Player
+  attr_accessor :score, :guess_word
+  attr_reader :name
+
   def initialize(name)
     @name = name
-    
+    @score = 0
+    @guess_word = ''
+  end
+
+  def make_guess
+    puts 'Enter your guess letter:'
+    input = gets.chomp.downcase
+    @guess_word << input
   end
 end
